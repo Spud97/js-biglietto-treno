3980db804ed3649a13c93608c4656162bc4c3dd5
Spud97 <luca.dercole97@gmail.com>
2022-05-18 16:19:29 +0200

    aggiorno ticket

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-const chiloMetri = prompt("Km da percorrere")
-const etaUtente = prompt("Età")
-const prezzoBiglietto = chiloMetri * 0.21;
+let chiloMetri = prompt("Km da percorrere")
+let etaUtente = prompt("Età")
+let prezzoBiglietto = chiloMetri * 0.21;
 
 if (etaUtente < 17) {
     prezzoBiglietto = ((chiloMetri * 0.21) - (((chiloMetri * 0.21) * 20) / 100))
 } else if (eta >= 65) {
     prezzoBiglietto = ((chiloMetri * 0.21) - (((chiloMetri * 0.21) * 40) / 100))
+} else {
+    prezzoBiglietto = chiloMetri * 0.21
 }
 
 chiloMetri = document.getElementById("km");
 etaUtente = document.getElementById("eta");
 prezzoBiglietto = document.getElementById("prezzo");
 
-chiloMetri.innerHTML = `${km}`
-etaUtente.innerHTML = `${eta}`
-prezzoBiglietto.innerHTML = `${prezzo}`
+chiloMetri.innerHTML = `${chiloMetri}`
+etaUtente.innerHTML = `${etaUtente}`
+prezzoBiglietto.innerHTML = `${prezzoBiglietto}`
 
 console.log (km, eta, prezzo)
