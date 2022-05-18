16eb21815905ce96bed786ec1bdb1f60d0530dab
Spud97 <luca.dercole97@gmail.com>
2022-05-18 16:44:57 +0200

    Aggiunto prezzo e sconti

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-let chiloMetri = prompt("Km da percorrere")
-let etaUtente = prompt("Età")
+const chiloMetri = prompt("Km da percorrere")
+const etaUtente = prompt("Età")
 let prezzoBiglietto = chiloMetri * 0.21;
 
-if (etaUtente < 17) {
-    prezzoBiglietto = ((chiloMetri * 0.21) - (((chiloMetri * 0.21) * 20) / 100))
-} else if (eta >= 65) {
-    prezzoBiglietto = ((chiloMetri * 0.21) - (((chiloMetri * 0.21) * 40) / 100))
+if (etaUtente < 18) {
+    prezzoBiglietto = ((chiloMetri * 0.21) - (((chiloMetri * 0.21) * 20) / 100)).toFixed(2)
+} else if (etaUtente >= 65) {
+    prezzoBiglietto = ((chiloMetri * 0.21) - (((chiloMetri * 0.21) * 40) / 100)).toFixed(2)
 } else {
-    prezzoBiglietto = chiloMetri * 0.21
+    prezzoBiglietto = (chiloMetri * 0.21).toFixed(2)
 }
 
-chiloMetri = document.getElementById("km");
-etaUtente = document.getElementById("eta");
-prezzoBiglietto = document.getElementById("prezzo");
+chiloMetriSegnati = document.getElementById("km");
+etaUtenteSegnata = document.getElementById("eta");
+prezzoBigliettoSegnato = document.getElementById("prezzo");
 
-chiloMetri.innerHTML = `${chiloMetri}`
-etaUtente.innerHTML = `${etaUtente}`
-prezzoBiglietto.innerHTML = `${prezzoBiglietto}`
+chiloMetriSegnati.innerHTML = `${chiloMetri}`
+etaUtenteSegnata.innerHTML = `${etaUtente}`
+prezzoBigliettoSegnato.innerHTML = `${prezzoBiglietto}`
 
 console.log (km, eta, prezzo)
